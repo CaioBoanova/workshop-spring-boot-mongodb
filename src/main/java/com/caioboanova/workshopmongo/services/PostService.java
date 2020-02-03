7fdbedb47304de4107217e15d282fe4df82207d0
CaioBoanova <caioaugusto.boanova@gmail.com>
2020-02-03 17:38:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/caioboanova/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.caioboanova.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caioboanova.workshopmongo.domain.Post;
+import com.caioboanova.workshopmongo.repository.PostRepository;
+import com.caioboanova.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
